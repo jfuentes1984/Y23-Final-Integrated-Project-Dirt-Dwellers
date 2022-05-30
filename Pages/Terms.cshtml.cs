d173f04fe420bce15c6ef7854342f1fc14c98bc2
jfuentes1984 <6179215@cegep-heritage.qc.ca>
2022-05-30 13:41:00 -0400

    Footer updated with privacy and terms

---
+++ Pages/Terms.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Y23_DirtDwellers.Pages;
+
+public class TermsofUseModel : PageModel
+{
+    private readonly ILogger<TermsofUseModel> _logger;
+
+    public TermsofUseModel(ILogger<TermsofUseModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
